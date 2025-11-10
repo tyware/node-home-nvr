f016a64b0cc8f97c67ad7a03a30b00f697b3ebc8
OPENTEXT\stian <stian@opentext.com>
2025-11-10 11:06:36 -0500

    2025-11-10 set 5

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nodejs npm
@@ -15,4 +17,15 @@ tar -xvzf libext/linux/libopencv411.tar.gz -C libext/linux
 chmod +x libext/linux/motion_server
 sudo apt-get update
 sudo apt-get install -y libgstreamer1.0-0 libgstreamer-plugins-base1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
-	  
+
+# Get current working directory	  
+CURRENT_DIR=$(pwd)
+# Replace the path in the service file
+sed -i "s|/home-nvr-path|$CURRENT_DIR|g" services/home.service
+sed -i "s|/home-nvr-path|$CURRENT_DIR|g" services/nvr.service
+sed -i "s|/home-nvr-path|$CURRENT_DIR|g" services/motion-server.service
+
+sudo cp -f services/home.service /etc/systemd/system/
+sudo cp -f services/nvr.service /etc/systemd/system/
+sudo cp -f services/motion-server.service /etc/systemd/system/
+
